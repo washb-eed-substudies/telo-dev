ed45730cdae7993a0c529a57257dd6c08484dcea
Sophia Tan <sophiatan@berkeley.edu>
2021-04-24 17:41:38 -0700

    updated main tables

--- tables/telo-dev-tables.R
+++ tables/telo-dev-tables.R
@@ -33,25 +33,25 @@ out_var <- c("Sum of 2nd, 4th, 5th, and 6th WHO motor milestones", "CDI comprehe
              "CDI comprehension Z-score Year 2", "CDI expressive language Z-score Year 2")
 
 tbl2 <- growth_tbl(" ", expo_var, out_var, exposure, outcome, unadj, adj, T)
-tbl2flex <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, T)
+tbl2flex <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, T, 1.5, 1.6)
 tbl2supp <- growth_tbl(" ", expo_var, out_var, exposure, outcome, unadj, adj)
-tbl2flexsupp <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj)
+tbl2flexsupp <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, F, .8, 1)
 
 
 #### Table 3 ####
 # concurrent y1 immune markers and y1 who motor milestones hazard ratios
 
 exposure <- c("TS_t2_Z") 
-outcome <- c("who_sit", "who_crawl", "who_stand_supp", 
+outcome <- c("who_crawl", "who_stand_supp", 
              "who_walk_supp", "who_stand_nosupp", "who_walk_nosup")
 expo_var <- c("Telomere length Z-score Year 1") 
-out_var <- c("Sitting without support", "Hands-and-knees crawling", "Standing with assistance",
+out_var <- c("Hands-and-knees crawling", "Standing with assistance",
              "Walking with assistance", "Standing alone", "Walking alone")
 
 tbl3 <- hr_tbl(" ", expo_var, out_var, exposure, outcome, HR, HRadj, T)
-tbl3flex <- hr_tbl_flex(" ", expo_var, out_var, exposure, outcome, HR, HRadj, T)
+tbl3flex <- hr_tbl_flex(" ", expo_var, out_var, exposure, outcome, HR, HRadj, T, 1.5, 1.3)
 tbl3supp <- hr_tbl(" ", expo_var, out_var, exposure, outcome, HR, HRadj)
-tbl3flexsupp <- hr_tbl_flex(" ", expo_var, out_var, exposure, outcome, HR, HRadj)
+tbl3flexsupp <- hr_tbl_flex(" ", expo_var, out_var, exposure, outcome, HR, HRadj, F, 1, .8)
 
 #### Table 4 ####
 # y2 telomeres and development
@@ -64,9 +64,9 @@ out_var <- c("EASQ communication Z-score", "EASQ gross motor Z-score", "EASQ per
              "CDI comprehension Z-score", "CDI expressive language Z-score") 
 
 tbl4 <- growth_tbl(" ", expo_var, out_var, exposure, outcome, unadj, adj, T)
-tbl4flex <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, T)
+tbl4flex <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, T, 1.5, 1.6)
 tbl4supp <- growth_tbl(" ", expo_var, out_var, exposure, outcome, unadj, adj)
-tbl4flexsupp <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj)
+tbl4flexsupp <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, F, .8, 1)
 
 #### Table 5 ####
 # change in telomere and development
@@ -77,9 +77,9 @@ outcome <- c("z_comm_easq", "z_motor_easq", "z_personal_easq", "z_combined_easq"
 expo_var <- c("Change in telomere length Z-score from Year 1 to Year 2") 
 out_var <- c("EASQ communication Z-score", "EASQ gross motor Z-score", "EASQ personal social Z-score", "EASQ combined Z-score",
              "CDI comprehension Z-score", "CDI expressive language Z-score") 
-
+    
 tbl5 <- growth_tbl(" ", expo_var, out_var, exposure, outcome, unadj, adj, T)
-tbl5flex <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, T)
+tbl5flex <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj, T, 1.6, 1.6)
 tbl5supp <- growth_tbl(" ", expo_var, out_var, exposure, outcome, unadj, adj)
 tbl5flexsupp <- growth_tbl_flex(" ", expo_var, out_var, exposure, outcome, unadj, adj)
 
@@ -101,10 +101,10 @@ save_as_docx("Table 1: Association Between Telomere Length at Year 1 and Child D
              "Table 2: Hazard Ratio for Motor Milestone Attainment for Telomere Length at Year 1" = tbl3flex, 
              "Table 3: Association Between Telomere Length at Year 2 and Child Development" = tbl4flex, 
              "Table 4: Association Between Change in Telomere Length and Child Development" = tbl5flex, 
-             path='C:/Users/Sophia/Documents/WASH/WASH Telomeres and Child Development/telo-dev main tables.docx')
+             path='C:/Users/Sophia/Documents/WASH/WASH Telomeres and Child Development/telo-dev main tables v2.docx', pr_section = sect_properties)
 
 save_as_docx("Table 1: Association Between Telomere Length at Year 1 and Child Development" = tbl2flexsupp, 
              "Table 2: Hazard Ratio for Motor Milestone Attainment for Telomere Length at Year 1" = tbl3flexsupp, 
              "Table 3: Association Between Telomere Length at Year 2 and Child Development" = tbl4flexsupp, 
              "Table 4: Association Between Change in Telomere Length and Child Development" = tbl5flexsupp, 
-             path='C:/Users/Sophia/Documents/WASH/WASH Telomeres and Child Development/telo-dev supplementary tables.docx')
+             path='C:/Users/Sophia/Documents/WASH/WASH Telomeres and Child Development/telo-dev supplementary tables v2.docx', pr_section = sect_properties)
